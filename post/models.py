2567399b0c76d742eee3d77387f887d272a0ab3e
Raasaal511 <raasaal511@gmail.com>
2023-02-04 10:21:37 +0300

    Created post and category models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('Title', max_length=150)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    discription = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField('Title', max_length=150)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
