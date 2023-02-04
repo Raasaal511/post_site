cac3102b1207bb7f6384842da5d7313a06c4db52
Raasaal511 <raasaal511@gmail.com>
2023-02-04 11:24:15 +0300

    created functions view home and post_detail

--- post/views.py
+++ post/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post, Category
+
+
+def home(request):
+    posts = Post.objects.select_related('category').all()
+
+    return render(request, '', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    return render(request, '', {'post': post})
