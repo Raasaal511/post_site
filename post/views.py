98d7ca4cc01fa111c30560f39145db329267df8c
Raasaal511 <raasaal511@gmail.com>
2023-02-05 11:49:43 +0300

    Added url to html file in function

--- post/views.py
+++ post/views.py
@@ -6,10 +6,10 @@ from .models import Post, Category
 def home(request):
     posts = Post.objects.select_related('category').all()
 
-    return render(request, '', {'posts': posts})
+    return render(request, 'main/home.html', {'posts': posts})
 
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
 
-    return render(request, '', {'post': post})
+    return render(request, 'main/post_detail.html', {'post': post})
