730c0cd3ade72379912bf469596ffe27bc7de89a
Raasaal511 <raasaal511@gmail.com>
2023-02-06 13:10:12 +0300

    Added the author of the model and model Comment

--- post/models.py
+++ post/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField('Title', max_length=150)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     discription = models.TextField()
@@ -19,3 +21,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment author: {self.author}'
