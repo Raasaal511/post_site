695688ef36d0e11518aa3cc290702b8f1eaef546
Raasaal511 <raasaal511@gmail.com>
2023-02-07 13:51:11 +0300

    In post_detail added form

--- post/views.py
+++ post/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 
+from .forms import CommentForm
 from .models import Post, Category
+from .services import save_comment
 
 
 def home(request):
     posts = Post.objects.select_related('category').all()
-
     return render(request, 'main/home.html', {'posts': posts})
 
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
+    comments = post.comments.all()
+    form = CommentForm()
 
-    return render(request, 'main/post_detail.html', {'post': post})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        save_comment(request, form, post)  # checked that the form is valid and will save the form
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'form': form
+    }
+
+    return render(request, 'main/post_detail.html', context=context)
