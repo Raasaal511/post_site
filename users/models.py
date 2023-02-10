6cce0267f3227e65d2a6a07af9aa3603190ce75b
Raasaal511 <raasaal511@gmail.com>
2023-02-10 22:31:16 +0300

    Created profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """User model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField('First name', max_length=50)
+    last_name = models.CharField('List name', max_length=50)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
