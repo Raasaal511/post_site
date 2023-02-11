32978c31424dcc763c1d2e4138cfa754a0e7f5ee
Raasaal511 <raasaal511@gmail.com>
2023-02-11 11:19:52 +0300

    Removed two attribute first_name, last_name

--- users/models.py
+++ users/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class Profile(models.Model):
     """User model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField('First name', max_length=50)
-    last_name = models.CharField('List name', max_length=50)
     bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
