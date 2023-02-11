e14f203b8f919c109d0ad32d3a0e4c83b2c5dc69
Raasaal511 <raasaal511@gmail.com>
2023-02-11 11:06:37 +0300

    Added users app in urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('post.urls'))
+    path('', include('post.urls')),
+
+    path('users/', include('users.urls')),
 ]
