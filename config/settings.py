dd963350d8af6d8ebf399f4ea1665a1201e188f0
Raasaal511 <raasaal511@gmail.com>
2023-02-11 11:03:30 +0300

    Added default login redirect url

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'users/profile/'
 
 # Application definition
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'post.apps.PostConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
