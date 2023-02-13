1c06dea0fd5e89f7cc7878b24135fd42ee7c27d1
Raasaal511 <raasaal511@gmail.com>
2023-02-13 17:01:29 +0300

    Create user login form

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,3 +7,8 @@ class UserCreateForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
