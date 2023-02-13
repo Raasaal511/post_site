26f05d8041e89b725490c3c0a89229ccd4810dcb
Raasaal511 <raasaal511@gmail.com>
2023-02-13 16:58:39 +0300

    Added post create, update, delete.

--- post/services.py
+++ post/services.py
@@ -1,4 +1,42 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render, get_object_or_404
+
+from post.forms import PostForm
+from post.models import Post
+
+
+def post_create(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+
+    return render(request, 'main/form/post_create.html', {'form': form})
+
+
+def post_udpate(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, instance=post)
+
+    if form.is_valid():
+        form.save()
+        return redirect('profile')
+
+    return render(request, 'main/form/post_udpate.html', {'post': post, 'form': form})
+
+
+def post_delete(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('profile')
+
+    return render(request, 'main/form/post_delete.html', {'post': post})
 
 
 def save_comment(request, form, post):
