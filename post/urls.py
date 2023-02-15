f2d83dc004982009b5f7e2716a03268ed694e5f3
Raasaal511 <raasaal511@gmail.com>
2023-02-15 13:45:49 +0300

    Added urls in post (create/update/delete) page

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from . import services as sc
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/', sc.post_create, name='post_create'),
+
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/update/', sc.post_udpate, name='post_update'),
+    path('<slug:slug>/delete/', sc.post_delete, name='post_delete'),
 ]
