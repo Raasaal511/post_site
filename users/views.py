006bfd960eede170116f8639a1f5c93cb9bf589d
Raasaal511 <raasaal511@gmail.com>
2023-02-15 13:49:12 +0300

    Create function profile_view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from post.models import Post
+from users.models import Profile
+
+
+def profile_view(request):
+    profile = Profile.objects.get(user=request.user)
+    user_posts = Post.objects.filter(author_id=profile.user.id)
+
+    context = {
+        'profile': profile,
+        'user_posts': user_posts,
+    }
+    return render(request, 'main/profile.html', context)
