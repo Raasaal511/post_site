50b2130386acbd8a22ebdadbf742f4eb44fd7da6
Raasaal511 <raasaal511@gmail.com>
2023-03-16 15:08:00 +0300

    Optimization sql reqest in Post model

--- post/views.py
+++ post/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 from .forms import CommentForm
-from .models import Post, Category
+from .models import Post
 from .services import save_comment
 
 
 def home(request):
-    posts = Post.objects.select_related('category').all()
+    posts = Post.objects.all().select_related('author', 'category').\
+        values('author__username', 'title', 'category__title', 'discription',
+               'create_at', 'slug')
     return render(request, 'main/home.html', {'posts': posts})
 
 
